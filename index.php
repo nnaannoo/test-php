dfaa037d8edf6a1d0469317556d0fb730398a5f9
nnaannoo <github@paskot.com>
2012-05-27 13:39:06 -0400

    first

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<?php
+
+/*%******************************************************************************************%*/
+// SETUP
+
+// Enable full-blown error reporting. http://twitter.com/rasmus/status/7448448829
+error_reporting(-1);
+
+// Include the SDK
+require_once '../test/AWSSDKforPHP/sdk.class.php';
+
+?>
+
+<html>
+<head>
+<title>PHP Test</title>
+</head>
+<body>
+	<?php echo '<p>Hello World</p>'; ?>
+	<?php echo '<p>Hola Mundo</p>'; ?>
+</body>
+</html>
